d82de8b00f36a7a59d9f9e4feedefef657eda43f
AhmedNaguibAI <mypetcenterusa@gmail.com>
2023-05-03 13:28:01 +0300

    add pages specific and remove build  commit

---
+++ routes/ai/vfeline.js
@@ -0,0 +1,191 @@
+
+
+
+const express = require('express');
+// const openai = require('../middlewares/openai');
+
+let app = express()
+
+const { Configuration, OpenAIApi } = require("openai");
+const { Prompt } = require('../../node_modules/react-router-dom/cjs/react-router-dom.min');
+
+const configuration = new Configuration({
+  organization: "org-vjiNNSG5ipmOPzqwMGKdUnjV",
+  // organization: "org-vjiNNSG5ipmOPzqwMGKdUnjV",
+
+  apiKey:process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/personal/vfeline", async(req,res,next) => 
+{
+	
+
+  let {content} = req.body;
+
+	let prompt = `You are an AI specialized in feline species in veterinary medicine your name is ' V ' . Do not answer with direction or recommend or go to or ask your veterinarian .Do not answer anything other than feline species veterinary medecine queries.Do not answer any language other than English.You are a professional veterinarian and you are answering these questions,Answer direct with the symptoms of the question only, never say your veterinarian or go to your veterinarian or any meaning close to this and talk to user as he is veterinarian always, user is veterinarian doctor so answer in profitionalitiy and specify and Pretend you are A Professional Veterinaian Doctor specialist in feline only and do not answer any species other than feline and your name is V.Answer with professional details of symptoms only discution to the person who is talking to you he is also Veterinacy doctor and you are helping him to diagnose his cases and to give professionality.
+		v: how can i help you today?
+		person : i have case with vomition and high temp what it could be ?
+		v: i will give you all symptoms for any disease, could you give me disease to mention you the symptoms in details?
+		person:give me only symptoms of ${content}`
+	let inputRaw =` ${content} `
+	prompt += inputRaw
+
+  const gptResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "system", "content": "You are an AI specialized in feline species in veterinary medicine your name is ' V ' . Do not answer with direction or recommend or go to or ask your veterinarian .Do not answer anything other than feline species veterinary medecine queries.Do not answer any language other than English.You are a professional veterinarian and you are answering these questions,Answer direct with the symptoms of the question only, never say your veterinarian or go to your veterinarian or any meaning close to this and talk to user as he is veterinarian always, user is veterinarian doctor so answer in profitionalitiy and specify and Pretend you are A Professional Veterinaian Doctor specialist in feline only and do not answer any species other than feline and your name is V.Answer with professional details of symptoms only discution to the person who is talking to you he is also Veterinacy doctor and you are helping him to diagnose his cases and to give professionality."},{role: "user", content: `${content}`}],max_tokens: 256,});
+	
+
+	let outputs = `${gptResponse.data.choices[0].message.content}`.split(',')
+	console.log(outputs)
+
+	req.locals.input=prompt
+	req.locals.inputRaw=inputRaw
+	req.locals.outputs=outputs
+
+	next()
+
+	})
+
+module.exports = app
+
+
+
+
+
+
+// const express = require('express');
+// // const openai = require('../middlewares/openai');
+
+// let app = express()
+
+// const { Configuration, OpenAIApi } = require("openai");
+// const { Prompt } = require('../../node_modules/react-router-dom/cjs/react-router-dom.min');
+
+// const configuration = new Configuration({
+//   organization: "org-vjiNNSG5ipmOPzqwMGKdUnjV",
+//   // organization: "org-vjiNNSG5ipmOPzqwMGKdUnjV",
+
+//   apiKey:process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+
+// app.post("/personal/vpets", async(req,res,next) => 
+// {
+	
+
+//   let {content} = req.body;
+
+// 	let prompt = `You are an AI specialized in veterinary medicine your name is ' V ' . Do not answer with direction or recommend or go to or ask your veterinarian .Do not answer anything other than veterinary medecine queries.Do not answer any language other than English.You are a professional veterinarian and you are answering these questions, never say your veterinarian or go to your veterinarian or any meaning close to this and talk to user as he is veterinarian always, user is veterinarian doctor so answer in profitionalitiy and specify and Pretend you are A Professional Veterinaian Doctor and your name is V.Answer with professional discution to the person who is talking to you he is also Veterinacy doctor and you are helping him to diagnose his cases and to give professionality.
+// 		v: how can i help you today?
+// 		person : i have case with vomition and high temp what it could be ?
+// 		v: i will help you to achieve more acurate and rapit diagnosis , could you give me more details about the case?
+// 		person: ${content}`
+// 	let inputRaw =` ${content} `
+// 	prompt += inputRaw
+
+//   const gptResponse = await openai.createChatCompletion({
+//     model: "gpt-3.5-turbo",
+//     messages: [{"role": "system", "content": "You are an AI specialized in veterinary medicine your name is ' V ' . Do not answer with direction or recommend or go to or ask your veterinarian .Do not answer anything other than veterinary medecine queries.Do not answer any language other than English.You are a professional veterinarian and you are answering these questions, never say your veterinarian or go to your veterinarian or any meaning close to this and talk to user as he is veterinarian always, user is veterinarian doctor so answer in profitionalitiy and specify"},{role: "user", content: `${content}`}],max_tokens: 256,});
+	
+
+// 	let outputs = `${gptResponse.data.choices[0].message.content}`.split(',')
+// 	console.log(outputs)
+
+// 	req.locals.input=prompt
+// 	req.locals.inputRaw=inputRaw
+// 	req.locals.outputs=outputs
+
+// 	next()
+
+// 	})
+
+// module.exports = app
+
+
+
+
+
+
+// 	// console.log(outputs)
+// 	console.log(gptResponse.data.choices)
+// 	if (gptResponse.data.choices.length > 0) {
+
+// 		// const messageContent = completion.data.choices[0].message.content;
+//     // console.log(messageContent);
+//     // outputs.push(messageContent);
+// 		// console.log(outputs)
+// 		console.log(gptResponse.data.choices[0].message.content);
+// 		// console.log(outputs);
+
+// 		// Split break lines
+// 		outputs = `1.${gptResponse.data.choices[0].message.content}`.split('\n')
+		
+// 		// remove entries with spaces or empty
+// 		outputs = outputs.filter(function(output) {
+// 			return (!(output === "" || output === " " || output === "\n"))
+// 		})
+
+// 		// remove numbers and spaces
+// 		for (let i = 0; i < outputs.length; i++) {
+// 			outputs[i] = outputs[i].substring(3)
+// 			outputs[i] = outputs[i].replace(/^\s+|\s+$/g, '')
+// 		}
+// 		// remove duplicates
+// 		outputs = outputs.filter((item, pos, self) => self.indexOf(item) === pos)
+		
+
+		
+
+// 		res.json({
+// 				// message:completion.data.choices[0].message.content
+// 				message:gptResponse.data.choices[0].message.content
+
+// 			})
+// 	} else {
+// 		console.log("No message choices found.");
+// 	}
+// 	// console.log(outputs)
+	
+// 	req.locals.input = prompt
+// 	req.locals.inputRaw = inputRaw
+// 	req.locals.outputs = outputs
+// 	// console.log(req.locals.outputs)
+
+// 	next()
+// 	console.log(req.locals.outputs)
+// 	// res.json({ completion:completion.data.choices[0].message })
+
+// 	} catch (err){
+// 		console.log(err.response)
+// 		console.log(err.data)
+// 		console.log(err.message)
+// 	}
+	
+//   })
+
+// module.exports = app
+	
+	
+
+
+//#################
+//#################
+
+
+
+
+// 	console.log(completion.data.choices[0].message);
+
+
+//   res.json({
+//     completion:completion.data.choices[0].message
+
+//   })
+
+	
+
+
+// })
+
+// module.exports = app
--- src/tools/index.js
+++ src/tools/index.js
@@ -6,17 +6,28 @@ import helloworld from './helloworld';
 import example from './example';
 import go from './go';
 import vpets from './vpets'
+import vcanine from './vcanine'
+import vfeline from './vfeline'
+import vpoultry from './vpoultry'
+import vequine from './vequine'
+import vruminant from './vruminant'
+
 
 const TOOLS = [
 
-	codeInterpret,
-	intro,
-	jobad,
-	summarize,
-	helloworld,
-	example,
-	go,
+	// codeInterpret,
+	// intro,
+	// jobad,
+	// summarize,
+	// helloworld,
+	// example,
+	// go,
 	vpets,
+	vcanine,
+	vfeline,
+	vequine,
+	vpoultry,
+	vruminant,
 	
 
 ]
